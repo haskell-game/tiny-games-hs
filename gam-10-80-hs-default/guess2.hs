652844b42a01b0c50c200edbfd761f794af775e6
Simon Michael <simon@joyful.com>
2023-02-07 01:44:50 -1000

    guess2: fix

--- gam-10-80-hs-default/guess2.hs
+++ gam-10-80-hs-default/guess2.hs
@@ -1,8 +1,8 @@
 #!/usr/bin/env stack
--- stack script --resolver=lts-20.9 --package random --verbosity=info
+-- stack script --resolver=lts-20.9 --package random --verbosity=error
 import System.IO (hFlush, stdout)
 import System.Random (randomRIO)
-main = do { let m = 10; n <- randomRIO (1,m::Int);
+main = let m = 10 in do { n <- randomRIO (1,m::Int);
  putStrLn $ "I'm thinking of a number between 1 and "++show m++", what is it ?";
  putStr "Guess: " >> hFlush stdout; g <- read <$> getLine; if g /= n
  then putStrLn ("Wrong! It was "++show n++". Try again.") >> main
