e20e4e96df7796d81d9a3090781ee7426697ce2e
Simon Michael <simon@joyful.com>
2023-02-07 08:44:10 -1000

    guess2: cleanup

--- default/guess2.hs
+++ default/guess2.hs
@@ -4,7 +4,7 @@ import System.IO (hFlush, stdout)
 import System.Random (randomRIO)
 main = let m = 10 in do { n <- randomRIO (1,m::Int);
  putStrLn $ "I'm thinking of a number between 1 and "++show m++", what is it ?";
- putStr "Guess: " >> hFlush stdout; g <- read <$> getLine; if g /= n
+ putStr "Guess: "; hFlush stdout; g <- read <$> getLine; if g /= n
  then putStrLn ("Wrong! It was "++show n++". Try again.") >> main
  else putStrLn "Right! You win." }
 
