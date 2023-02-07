31e50400b7aee2a87113e58b7e04a30ba3b91a2a
Simon Michael <simon@joyful.com>
2023-02-07 10:59:00 -1000

    consistent ghc note placement

--- base/template1.hs
+++ base/template1.hs
@@ -1,4 +1,4 @@
-#!/usr/bin/env runghc   # ghc 9.2.5, gam-10-80-hs-base/template1 (mynick)
+#!/usr/bin/env runghc   # gam-10-80-hs-base/template1 (mynick), ghc 9.2.5
 import Text.Printf
   -- https://hackage.haskell.org/package/base/docs/Text-Printf.html
 main = do
--- prelude/guess1.hs
+++ prelude/guess1.hs
@@ -1,4 +1,4 @@
-#!/usr/bin/env runghc   # ghc 8.10.3, gam-10-80-hs-prelude/guess1 (sm)
+#!/usr/bin/env runghc   # gam-10-80-hs-prelude/guess1 (sm), ghc 8.10.3
 main = do
   putStrLn "what number am I thinking of ? "
   getLine
