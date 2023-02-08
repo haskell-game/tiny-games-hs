3d556f31e0c50ae1681e8df19db703679d9c1ba5
Travis Cardwell <travis.cardwell@extrema.is>
2023-02-08 20:19:52 +0900

    Add base/timing and hackage/guesscolor

---
+++ base/timing/timing.hs
@@ -0,0 +1,10 @@
+#!/usr/bin/env stack
+-- stack script --resolver=lts-20.10
+import GHC.Clock (getMonotonicTime)
+import System.Environment (getArgs)
+main = do -- gam-10-80-hs-base/timing (TravisCardwell)
+  secs <- read . head . (++ ["30"]) <$> getArgs
+  putStrLn $ "Press enter.  Wait " ++ show secs ++ " seconds.  Press enter."
+  (t0,t1) <- (,) <$> (getLine>>getMonotonicTime) <*> (getLine>>getMonotonicTime)
+  mapM_ putStrLn $ ("You waited " ++ show (t1 - t0) ++ " seconds.")
+    : ["Score: " ++ show (100 * (1 - min 1 (abs (t0 + secs - t1) / secs)))]
