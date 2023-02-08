db2446e2755cfa5cbf2d019200ee978c21b2de64
Simon Michael <simon@joyful.com>
2023-02-08 05:27:52 -1000

    ski: success, managed to make it a stack script again

--- hackage/ski/ski.hs
+++ hackage/ski/ski.hs
@@ -1,20 +1,15 @@
-import Import
-s=78;ll=6;rl=s-6;c=s`div`2;w=24;m=2;t='🌲';h=(`div`2);q=p.show;ra=randomRIO
-rp=replicate;rm=replicateM_;d=threadDelay 50000;p s=putStrLn s>>d;sc=rm 10$p""
-main=hSetEcho stdin False>>hSetBuffering stdin NoBuffering>>p"Ski!">>lp 0 c c
-lp o x c=do{let{l0=c-h w;(l,ml)=if l0<ll then(ll,0)else(l0,m);r0=c+h w+1;(r,mr)=
- if r0>rl then(rl,0)else(r0,m);};j<-hReady stdin;i<-(if j then getChar else
- return '-');let{lt=h$l-1;rt=h$s-r-1;ls=x-lt*2;rs=s-rt*2-x-1;k=if min ls rs<1
- then"*"else"V"} in do {p(take s$rp lt t++rp ls ' '++k++rp rs ' '++rp rt t);if k
- /="*"then lp(o+1)(case i of ','->x-1;'.'->x+1;_->x).(c+)=<<ra(-ml,mr)else q o}}
--- these lines freed up by
--- abandoning shebang line
+#!/usr/bin/env stack
+-- stack script --compile --resolver=lts-20.10 --package random
+import Import;s=78;ll=6;rl=s-6;c=s`div`2;w=24;m=2;t='🌲';h=(`div`2);q=p.show
+ra=randomRIO;rp=replicate;rm=replicateM_;d=threadDelay 50000;p s=putStrLn s>>d
+sc=rm 10$p"";main=hSetEcho stdin False>>hSetBuffering stdin NoBuffering>>p"Ski!"
+ >>lp 0 c c;lp o x c=do{let{l0=c-h w;(l,ml)=if l0<ll then(ll,0)else(l0,m);r0=c+h
+w+1;(r,mr)=if r0>rl then(rl,0)else(r0,m);};j<-hReady stdin;i<-(if j then getChar
+else return '-');let{lt=h$l-1;rt=h$s-r-1;ls=x-lt*2;rs=s-rt*2-x-1;k=if min ls rs
+<1 then"*"else"V"} in do {p(take s$rp lt t++rp ls ' '++k++rp rs ' '++rp rt t);if
+k/="*"then lp(o+1)(case i of ','->x-1;'.'->x+1;_->x).(c+)=<<ra(-ml,mr)else q o}}
 -- ^10 ------------------------------------------------------------------ 80> --
-{- gam-10-80-hs-hackage/ski (sm)
-
-stack ghc --resolver=lts-20.9 --package random ski.hs
-
-Avoid trees. Player controls: , . 
+{- gam-10-80-hs-hackage/ski (sm). Avoid trees. Player controls: , . 
 
 NB: for me the 🌲 emoji renders very slowly in some terminals (iTerm, Terminal),
 but normally in VS Code terminal.
