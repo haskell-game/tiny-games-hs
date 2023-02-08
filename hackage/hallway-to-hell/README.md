d9581470aac19d3caae99cf383ff3151b5fbf56f
Julien Dehos <dehos@univ-littoral.fr>
2023-02-08 18:04:40 +0100

    hallway-to-hell

---
+++ hackage/hallway-to-hell/README.md
@@ -0,0 +1,15 @@
+# hallway-to-hell
+
+## Build & run
+
+```
+stack --resolver lts-18 script --package "rio random" --compile hallway-to-hell.hs
+./hallway-to-hell
+```
+
+## The game
+
+Use the a/d keys to move to the left/right.
+
+![](hallway-to-hell.gif)
+
---
+++ hackage/hallway-to-hell/hallway-to-hell.hs
@@ -0,0 +1,10 @@
+import RIO
+import System.Random
+move k c = if c=='a' then k-1 else if c=='d' then k+1 else k
+run v b = threadDelay 200000 >> putStr "\ESC[2J" >> readIORef v >>= \k -> mapM_ 
+  putStrLn("\n":b++[replicate k ' '++"^"])>>when(last b!!k==' ') (up b>>=run v)
+up b = (\t -> ("X" ++ f t (init $ tail $ head b)++ "X") : init b) <$> randomIO
+  where f t hb = if t then "X"++init hb else tail hb++ "X"
+key v = readIORef v >>= \k -> getChar >>= writeIORef v . move k>> key v
+main = hSetBuffering stdin NoBuffering >> newIORef 11 >>= \v->
+  concurrently_ (key v) (run v $ replicate 10 $ "X" ++ replicate 21 ' ' ++ "X")
