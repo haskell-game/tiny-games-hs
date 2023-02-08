404abc4180c060bf0fcc56ca6e07d9446ab0e06d
Simon Michael <simon@joyful.com>
2023-02-08 09:34:57 -1000

    hallway-to-hell: add build instruction to the file itself, per rules

--- hackage/hallway-to-hell/hallway-to-hell.hs
+++ hackage/hallway-to-hell/hallway-to-hell.hs
@@ -8,3 +8,9 @@ up b = (\t -> ("X" ++ f t (init $ tail $ head b)++ "X") : init b) <$> randomIO
 key v = readIORef v >>= \k -> getChar >>= writeIORef v . move k>> key v
 main = hSetBuffering stdin NoBuffering >> newIORef 11 >>= \v->
   concurrently_ (key v) (run v $ replicate 10 $ "X" ++ replicate 21 ' ' ++ "X")
+-- ^10 ------------------------------------------------------------------ 80> --
+{- gam-10-80-hs-hackage/hallway-to-hell (juliendehos)
+
+stack runghc hallway-to-hell.hs --resolver lts-20.10 --package rio --package random
+
+-}
