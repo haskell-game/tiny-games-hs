8b1733ebc352cafd561e1019f32256d9b13d4387
Samuel Gélineau <gelisam+github@gmail.com>
2023-02-08 21:46:03 -0500

    1234-hero: high score and miss feedback

--- hackage/1234-hero/Imports.hs
+++ hackage/1234-hero/Imports.hs
@@ -1,9 +1,11 @@
 module Imports
-  ( ord
+  ( guard
+  , ord
   , getStdGen, randomRs
   , play, Display(InWindow), white, translate, scale, rectangleWire, text
   , rectangleSolid, Event(EventKey), KeyState(Down), Key(Char)
   ) where
+import Control.Monad
 import Data.Char
 import System.Random
 import Graphics.Gloss.Interface.Pure.Game
