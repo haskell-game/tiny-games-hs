0c7299ef9716fdffe50313b556a2f30df7be6964
Simon Michael <simon@joyful.com>
2023-02-09 08:52:59 -1000

    rhythm: avoid env -S

--- base/rhythm/rhythm.hs
+++ base/rhythm/rhythm.hs
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S runghc # gam-10-80-hs-base/rythm (elderephemera), ghc 9.0, 9.2
+#!/usr/bin/env runghc
 {import GHC.Conc;import System.IO;import System.CPUTime;i r|r>9="*"|let="";t=r w
 ;o a d p r(n:m)b=do{u$"\ESC[2J"++unlines(reverse p++("-O-O-O-O- "++show a++i r):
 (n:m));threadDelay d;q<-g;u<-getCPUTime;let{k=q==filter(/=w)n;x|n==t=0|r>9,k=2|k
@@ -9,3 +9,4 @@
 p"Keyset(0-2):\n";hSetEcho l$0==1;hSetBuffering l NoBuffering;o 0((10-d)*97897)[
 ]0(r t)$[" w a d s "," j k l ; "," 8 4 6 2 "]!!f};z=getChar;w=' ';r=replicate 9}
 -- ^10 ------------------------------- 80> -------------------------------------
+-- gam-10-80-hs-base/rythm (elderephemera), ghc 9.0, 9.2
