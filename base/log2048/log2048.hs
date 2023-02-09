ac942cf7223b95e4743bd9dd894b77e73bb227b5
Xia Li-yao <Lysxia@users.noreply.github.com>
2023-02-09 01:48:43 +0100

    Fix off-by-one in log2048

--- base/log2048/log2048.hs
+++ base/log2048/log2048.hs
@@ -5,7 +5,7 @@ f x k = foldr (\j -> if k == j then g . r else r) x "hjlk"
 h u x = s u x id where{ s i [] k | i == u = error "You lose!" | True = h i x ;
 s 0 (0 : t) k = k (1 : t) ; s i (0 : t) k = s (i-1) t (k . (0 :)) ; s i (j : t)
 k = s i t (k . (j :))} ; e = map (take 4) . take 4 . iterate (drop 4)
-l (y : z) x | (_ : _) <- filter (== 11) (concat x) = q x >> putStrLn "You won!"
+l (y : z) x | (_ : _) <- filter (== 12) (concat x) = q x >> putStrLn "You won!"
   | True = do{q x ; getLine >>= l z . e . h y . concat . f x . head}
 main = print "keys: hjkl">>(l (cycle [3,14,15,9,2,65,3,5]) . e . h 2 . repeat) 0
 -- ^10 ------------------------------------------------------------------ 80> --
