9204cd6ce5b1c7658b3bbd5095cf3d1bfb23617d
migmit <migmit@gmail.com>
2023-02-09 21:35:21 +0100

    Another step

--- prelude/marriages/marriages.hs
+++ prelude/marriages/marriages.hs
@@ -1,35 +1,35 @@
-#!/usr/bin/env runghc -XLambdaCase
-{-# LANGUAGE LambdaCase #-}
+#!/usr/bin/env -S runghc -cpp -DD=a=replicate;b=putStrLn;c=length;p=map;u=max(2)
+main = n $ p (`div` 2 ^ 16) $ iterate (\k -> mod (k * 22695477 + 1) (2 ^ 32)) 9
 
-m = ["Adam ", "Bela ", "Csaba", "David", "Edgar", "Felix", "Geza ", "Hugo ", "Imre "]
-
-f = ["Judit", "Klara", "Lujza", "Maria", "Nora ", "Oxana", "Paula", "Rozsa"]
-
-z b x y =
-  if b
-    then x
-    else y
-
-h m f l = y (++) (a 6 "     " ++ m) $ p (s ' ') $ x (y (:)) (a 5 "") f ++ "" : p (p (" .v" !!)) l
-
-d x = c . takeWhile ((x /=) . head)
-
-e i = (\(l, _:r) -> l ++ r) . t i
-
-w m f l =
-  let q = getLine >>= \r@(~[y, x]) -> (\i j -> z (c r /= 2 || l !! j !! i < 2) q $ w (e j m) (e i f) (e i `p` e j l)) (d x f) (d y m)
-   in z (all (all (2 >)) l) (b $ z (f == []) "SUCCESS" "FAIL") $ mapM b (h m f l) >> q
-
-i j c = ((\(l, v:r) -> l ++ c v : r) .) . t . mod j
+n r = (\(l, q) -> w 8 l >> b (a 40 '=') >> n q) $ g r $ a 10 $ a 8 1 ++ [3]
 
 g (o:p:q:r) l =
-  z (mod o 10 == 0 && all (\s -> c [k | k <- l, any ((1 <) . (k !!)) s] >= c s) (x (\r -> (>>= \q -> [r : q, q])) [[]] [0 .. 7])) (l, r) $
-  g r $ i p (i q (\_ -> 2) 8) 9 l
+  z (o < 6555 && all (\s -> c [k | k <- l, any ((1 >) . (k !!)) s] >= c s) (foldr (\r -> (>>= \q -> [r : q, q])) [[]] [0 .. 7]))
+    [g r $ i p (i q (* 0) 8) 9 l, (l, r)]
 
-main =
-  let n r = (\(l, q) -> w m f l >> b (a 40 '=') >> n q) $ g r $ a 10 $ a 8 1 ++ [0]
-   in n $ p (`div` 2 ^ 16) $ iterate (\k -> mod (k * 22695477 + 1) (2 ^ 32)) 9
+w n l = z (all (all (0 <)) l) [mapM b (h l) >> q n l, b $ z (n < 1) ["LOSS", "WIN"]]
 
+z b = (!! fromEnum b)
+
+h l = zipWith (++) (a 6 "     " ++ m) $ p (s ' ') $ f ++ "" : p (p ("X.O " !!)) l
+
+d x = c . takeWhile (x /=)
+
+q n l =
+  getLine >>= \r@(~[y, x]) ->
+    (\k j -> z (c r /= 2 || l !! j !! k > 0) [w (n - 1) (p (i k u 8) $ i j (p u) 9 l), q n l]) (d x $ f !! 0) (d y $ p (!! 0) m)
+
+i j c = ((\(l, v:r) -> l ++ c v : r) .) . splitAt . mod j
+
+f = y "JKLMNOPR\nuluaoxao\ndajrrauz\nirzianls\ntaaa aaa"
+
+s c = (c :) . (>>= (: [c]))
+
+m = y "Adam \nBela \nCsaba\nDavid\nEdgar\nFelix\nGeza \nHugo \nImre "
+
+y = lines
+
+--{-
 a = replicate
 
 b = putStrLn
@@ -38,10 +38,12 @@ c = length
 
 p = map
 
-t = splitAt
-
-y = zipWith
-
-x = foldr
-
-s c = (c :) . (>>= (: [c]))
+u = max (2)
+-- >>> fromEnum 'A'
+-- 65
+-- >>> fromEnum 'J'
+-- 74
+-- >>> :t (<$>)
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+--
+--}
