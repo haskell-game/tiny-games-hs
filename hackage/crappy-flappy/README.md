2e0e54a00527a8c287b2ebef07dc5d0686481cae
Gergo ERDI <gergo@erdi.hu>
2023-02-09 20:21:04 +0800

    Add Crappy Flappy

---
+++ hackage/crappy-flappy/README.md
@@ -0,0 +1,14 @@
+% Crappy Flappy
+
+## Build & run
+
+```
+./crappy-flappy
+```
+
+## The game
+
+Press a key to flap upwards. Try to get to the right edge of the screen.
+
+![](crappy-flappy.gif)
+
---
+++ hackage/crappy-flappy/crappy-flappy.hs
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S stack script --resolver=lts-19.21 --package ansi-terminal-game
+-- stack script --ghc-options -threaded
+import Terminal.Game;end ps (y,dy,x)=y>120||y<0||x>3950||
+  (x`mod`40==0&&let(t,b)=ps!!(x`div`40)in y`div`5<t||y`div`5>(t+b)); main=do
+{let _:(ps@((y0,_):_))=iterate(\(x,y)->(3+((x+9)`mod`7),5+((y+12)`mod`5)))(3,3)
+;playGame(Game 20 (y0+1,0,1) logic (draw (take 10 ps)) (end ps));}
+logic _ (y,dy,x) e=case e of { Tick -> (y+dy`div`2,dy+1,x+1); _ -> (y-3,0,x+1)}
+draw p r(y,_,x)=mergePlanes(blankPlane 80 24&(y`div`5,x`div`5)%cell '@')$concat
+  [[((0,x*8),box 1 t '|'),((t+b,x*8),box 1(24-t-b) '|')]|(x,(t,b))<-zip[0..]p]
+
+-- ^10 ------------------------------------------------------------------ 80> --
+{- gam-10-80-hs-hackage/crappy-floppy (gergoerdi)
+
+-}
