6383f99cd8a96c5223887e8c27dedd0df3c45337
Gergo ERDI <gergo@erdi.hu>
2023-02-09 21:24:35 +0800

    Randomize pipes

---
+++ hackage/crappy-flappy/Imports.hs
@@ -0,0 +1,3 @@
+module Imports(module Terminal.Game, module GHC.Clock) where
+import Terminal.Game
+import GHC.Clock
--- hackage/crappy-flappy/crappy-flappy.hs
+++ hackage/crappy-flappy/crappy-flappy.hs
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S stack script --resolver=lts-20.10 --package ansi-terminal-game
 -- stack script --ghc-options -threaded
-import Terminal.Game;end ps (y,dy,x)=y>120||y<0||x>3950||
+import Imports;end ps (y,dy,x)=y>120||y<0||x>3950||
   (x`mod`40==0&&let(t,b)=ps!!(x`div`40)in y`div`5<t||y`div`5>(t+b)); main=do
-{let _:(ps@((y0,_):_))=iterate(\(x,y)->(3+((x+9)`mod`7),5+((y+12)`mod`5)))(3,3)
+{s<-fromIntegral.(`mod`100)<$>getMonotonicTimeNSec
+;let _:(ps@((y0,_):_))=iterate(\(x,y)->(3+((x+9)`mod`7),5+((y+12)`mod`5)))(s,s)
 ;playGame(Game 20 (y0+1,0,1) logic (draw (take 10 ps)) (end ps));}
 logic _ (y,dy,x) e=case e of { Tick -> (y+dy`div`2,dy+1,x+1); _ -> (y-3,0,x+1)}
 draw p r(y,_,x)=mergePlanes(blankPlane 80 24&(y`div`5,x`div`5)%cell '@')$concat
   [[((0,x*8),box 1 t '|'),((t+b,x*8),box 1(24-t-b) '|')]|(x,(t,b))<-zip[0..]p]
-
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-hackage/crappy-floppy (gergoerdi)
 
