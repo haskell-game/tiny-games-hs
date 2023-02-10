b6f06399e92d218631246e7f9fa48d53f4aba168
Simon Michael <simon@joyful.com>
2023-02-09 15:54:34 -1000

    bulls-n-cows: make it executable, since there's room for now
    Excuse the fixup

--- hackage/bulls-n-cows/bulls-n-cows.hs
+++ hackage/bulls-n-cows/bulls-n-cows.hs
@@ -1,3 +1,5 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-20 --package random
 import Control.Monad (when);import System.Random (uniformR, newStdGen)
 l=length;f=sum. fmap fromEnum;p=putStrLn;(x:xs)!!!0=(x,xs);(x:xs)!!!n=(x:)<$>(xs
  !!!(n-1));r n x g=let{(i,h)=uniformR(0,l x-1)g;(z,y)= x!!!i}in if n==1 then[z]
@@ -6,11 +8,6 @@ else z:r(n-1)y h;main=turn 10. r 4['0'..'9']=<<newStdGen;turn 0 s=p"You lost :("
  ->do if(l g/=4)then do{p$"You need to guess 4 digits";turn a s}else do{let{b=f$
 zipWith(==)g s;c=f((flip elem g)<$>s)-b};if b==4 then p"You won!"else do{p(show
 b<>" bulls and "<>show c<>" cows");turn(a-1)s}}
-
-
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-hackage/bullsandcows (akadude)
-
-stack runghc --resolver lts-20.10 --package random bullsandcows.hs
-
 -}
