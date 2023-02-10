952ffe7057d7d81c813ecb9535ef571c5dcb141e
migmit <migmit@gmail.com>
2023-02-10 13:28:14 +0100

    Kiara->Klara

--- prelude/matchmaking/matchmaking.hs
+++ prelude/matchmaking/matchmaking.hs
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S runghc -cpp -DD=a=replicate;b=putStrLn;c=length;p=map;u=max(2)
 m=y"Adam \nBela \nCsaba\nDavid\nErik \nFeri \nGeza \nImre \nJeno ";w 0_=b"WIN";w
  n l=z(all(all(0<))l)[mapM b(h l)>>q n l,b"LOSS"];d x=c.takeWhile(x/=);y=lines;D
-f=y"HKLMNPRS\naiuaoeoa\nnajjrtzr\nnrzaaraa\naaa  a  ";z b=(!!fromEnum b);main=n$
+f=y"HKLMNPRS\naluaoeoa\nnajjrtzr\nnrzaaraa\naaa  a  ";z b=(!!fromEnum b);main=n$
  p(`div`2^16)$iterate(\k->mod(k*22695477+1)(2^32))9;h l=zipWith(++)(a 5"     "++
  m)$p((' ':).(>>=(:" ")))$f++p(p("O.X "!!))l;q n l=getLine>>= \r@(~[y,x])->(\k j
  ->z(c r/=2||l!!j!!k>0)[w(n-1)(p(i k u 8)$i j(p u)9l),q n l])(d x$f!!0)(d y$p(!!
