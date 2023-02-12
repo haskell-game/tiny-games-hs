594b501e171fe02ae998f34d5062a89ff6e70ad9
Francesco Gazzetta <fgaz@fgaz.me>
2023-02-12 15:08:12 +0100

    brickbreaker: v2
    * less, bigger bricks
    * the ball bounces on bricks
    * playing area delimiter
    * win/lose screen (well, color)

--- hackage/brickbreaker/brickbreaker.hs
+++ hackage/brickbreaker/brickbreaker.hs
@@ -1,18 +1,23 @@
 #!/usr/bin/env -S stack script --compile --resolver lts-20 --package gloss
-import Graphics.Gloss;import Graphics.Gloss.Interface.IO.Interact;main=play
- FullScreen white 60(0,(0,0),(0.2,0.2),[(x,y)|x<-[0,2..20],y<-[0,2..8]])render
- input step;scaleFactor=20;render(p,(x,y),_,bricks)=scale scaleFactor
- scaleFactor$line[(p-2,-10),(p+2,-10)]<>translate x y(circle 1)<>foldMap(\(bx,
- by)->polygon[(bx,by),(bx+1,by),(bx+1,by+1),(bx,by+1)])bricks;input(EventMotion
- (p,_))(_,ball,ballV,bricks)=(p/scaleFactor,ball,ballV,bricks);input _ s=s;
-step t (p,(x,y),(v,w),bricks)=(p,(x+v,y+w),(v',w'),filter(\(bx,by)->bx<x||bx>x+
- 2||by<y||by>y+2)bricks)where{(v',w')|y< -10&&y> -11&&x>p-2&&x<p+2=
- ((x-p)/10,abs w)|x<0||x>20=(-abs v*signum x,w)|y>10=(v,-abs w)|otherwise=(v,w)}
+import Graphics.Gloss;import Graphics.Gloss.Interface.IO.Interact;r=20;main=play
+ FullScreen white 60(0,(0,-20),(8,16),[(x,y)|x<-[0,2..20],y<-[2,4..8]]) f g h; o
+ b c|b=color c$thickCircle 1 99|True=blank;f(p,(x,y),_,bs)=scale r r$o(y< -20)
+ red<>o(null bs)green<>line[(0,10),(22,10),(22,-11),(0,-11),(0,10)]<>line[(p-2,-
+ 10),(p+2,-10)]<>translate x y(circle 1)<>foldMap(\(x,y)->polygon[(x,y),(x+1.8,y
+ ),(x+1.8,y+1.8),(x,y+1.8)])bs;g(EventMotion(p,_))(_,b,bv,bs)=(p/r,b,bv,bs);g _
+ s=s;h t(p,(x,y),(v,w),bs)=(p,(x+v*t,y+w*t),(v',w'),bs')where{bs'=filter(\(bx,by
+ )->bx<x||bx>x+2||by<y||by>y+2)bs;(v',w')|y< -10&&y> -11&&x>p-2&&x<p+2=((x-p)*10
+ ,abs w)|x<0||x>20=(-abs v*signum x,w)|y>10||bs/=bs'=(v,-abs w)|True=(v,w)}
 -- ^10 ------------------------------------------------------------------ 80> --
 {- hackage-10-80/brickbreaker (fgaz)
 
 Move the pointer to move the paddle.
 The ball is lost if it moves past the paddle.
 Try to destroy all bricks!
+
+Tip: the bottom of the ball has magical destructive properties, try to get it
+above the bricks to destroy many at once.
+
+Adjust the `r` variable if the game is too big or too small for your screen
 
 -}
