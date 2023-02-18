9dc5d4e378e61c298ec16151b9a03a3f93db8b25
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2023-02-18 17:33:42 -0400

    Add short-guess game

---
+++ default/short-guess/short-guess.hs
@@ -0,0 +1,12 @@
+#!/usr/bin/env runhaskell
+import System.Random (randomRIO);w = ["cute","game","play","dream"];n wr f = do
+  putStrLn $ map (\i -> if i `elem` f then i else '_') wr
+  if (length wr == length f) then putStrLn "You won!"
+  else getLine >>= \i ->  (if (i !! 0) `elem` wr then do
+      putStrLn "You got it"
+      n wr (i !! 0 : f)
+    else putStrLn "try again" >> n wr f)
+main=putStrLn "Guess the word!" >> randomRIO (0,length w - 1::Int) >>= \r -> do
+  return (w !! r) >>= \wr -> n wr [];
+-- ^10 ------------------------------------------------------------------ 80> --
+{- default-10-80/short-guess (RimuhRimu). random -}
