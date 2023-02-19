8a511d08b0a4ab14a4347e7425aa5c5cf6926a9a
Akshay Mankar <itsakshaymankar@gmail.com>
2023-02-19 14:55:54 +0100

    7up7down (akshaymankar)

---
+++ hackage/7up7down/7up7down.hs
@@ -0,0 +1,24 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-20 script --package random --compile
+import System.Random;import Data.Bifoldable;import System.IO;d n=putStr$toEnum
+ <$>[9855+n,32];main=g 10;g b=if(b<=0)then putStrLn"c ya l8r l0$3r"else putStr(
+ "balance=₹"++(show b)++"|bet=₹1|7/7up/7down/q: ")>>hFlush stdout>>getLine>>= \i
+ ->if i/="q"then(,)<$>randomRIO(1,6)<*>randomRIO(1,6)>>= \(x,y)->bimapM_((d x>>d
+ y>>).putStrLn)g$if(x+y>7&&i=="7up")||(x+y<7&&i=="7down")||(x+y==7&&i=="7")then(
+ "You win!",b+1+fromEnum(i=="7"))else("You lose!",(b-1)) else putStrLn"Bye!"
+-- ^08 ------------------------------------------------------------------ 80> --
+--
+-- Category: hackage-10-80
+-- Tested with: GHC 9.2.6
+-- Author: @akshaymankar
+--
+-- Guess the result of rolling two 6 sided dice by writing one of 7up, 7down or
+-- 7.
+-- 7up wins if the result greater than 7
+-- 7down wins if the result is less than 7
+-- 7 wins if the result is exactly 7.
+-- Starting balance is ₹10, every bet is ₹1.
+-- On victory with 7up or 7down, the player wins ₹1
+-- On victory with a 7, the player wins ₹2.
+-- Enter q instead of a guess to exit.
+-- Any invalid inputs (including typos) lose.
