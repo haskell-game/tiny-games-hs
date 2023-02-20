3cc974dcb53ddc0efaf8d69b0d20710f672d9dd0
Gergo ERDI <gergo@erdi.hu>
2023-02-20 22:32:11 +0800

    We can finally do nice discrete blocks now!

--- hackage/tetris/tetris.hs
+++ hackage/tetris/tetris.hs
@@ -2,7 +2,7 @@
 import Imports;main=playGame(Game 6(p!!0,r 20 0)l d(\_->False));z=zipWith;p=[[[
  120],[32,32,32,32]],[[32,112],[32,48,32],[112,32],[32,96,32]],l,m l,z,m z,[[48,
  48]]]where{m=h reverse;l=[[16,112],[32,32,48],[112,64],[48,16,16]];z=[[24,48],[
- 32,48,16]]};u=unlines.h(\r->foldMap(bool "  ""██".testBit (r*8))[0..14]);a(p,f)
+ 32,48,16]]};u=unlines.h(\r->foldMap(bool "  ""▇▉".testBit (r*8))[0..14]);a(p,f)
  =t((p!!0)++repeat 0)f;w=t(r 19 2049++[4095]);r=replicate;o(p,f)=(tail.cycle$p,f
  );l _ s(KeyPress k)|k=='k',let y=o s,c y=y|k=='j',let y=m(`div`2)s,c y=y|k=='l'
  ,let y=m(*2)s,c y=y;l _ s _|let y=f s,c y=y|True=g s;d _ s=stringPlane$u(w$a s)
