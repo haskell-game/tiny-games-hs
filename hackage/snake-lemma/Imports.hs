d0765f5de96b5363ad53ce464840f3bb61ab3a4f
Gergo ERDI <gergo@erdi.hu>
2023-02-21 22:12:26 +0800

    Initial "clean" version of Snake Lemma

---
+++ hackage/snake-lemma/Imports.hs
@@ -0,0 +1,11 @@
+module Imports
+  ( module Data.Bits
+  , module Data.Bool
+  , module Data.List
+  , module Terminal.Game
+  ) where
+
+import Data.Bits
+import Data.Bool (bool)
+import Data.List (partition)
+import Terminal.Game
