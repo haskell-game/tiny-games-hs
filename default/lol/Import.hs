21ee2daf188ff70b78399e5fcb8beb7b3dae34e0
Miao, ZhiCheng <miao@superfluid.finance>
2023-02-22 13:32:40 +0200

    remove some unused imports

--- default/lol/Import.hs
+++ default/lol/Import.hs
@@ -1,10 +1,8 @@
 module Import(module X) where
 
-import           Control.Monad            as X (unless)
 import           Data.Bool                as X (bool)
 import           Data.Char                as X (isLetter)
 import           Data.List                as X (intercalate)
-import           Data.Maybe               as X (isJust)
 import           Data.Text                as X (pack, unpack)
 import           Data.Text.IO             as X (hGetChunk, hPutStr)
 import           System.Environment.Blank as X (getArgs, getEnvDefault)
