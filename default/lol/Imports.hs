59426c0c761247a729a475b235fe4f0580326de0
Miao, ZhiCheng <miao@superfluid.finance>
2023-02-22 13:14:01 +0200

    lol: performance improvements & bug fixes
    - Use Data.Text.IO for better performance in slower terminals (courtesy to TristanC).
    - Fixed a bug dealing with leftover chars from previous @p@ interation.
    - hFlush issue for some game (7up7down).
    - true infinite (notice the rainbow frequency changes over lols).

--- default/lol/Imports.hs
+++
@@ -1,8 +0,0 @@
-module Imports(module X) where
-
-import           Control.Monad            as X
-import           Data.Char                as X
-import           Data.List                as X
-import           GHC.IO.Handle            as X
-import           System.Environment.Blank as X
-import           System.Process           as X
