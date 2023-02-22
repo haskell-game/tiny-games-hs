6dd81631ee5933110337d38eeffefad82ceb30cc
Simon Michael <simon@joyful.com>
2023-02-21 17:38:22 -1000

    balances: updates, docs; more usable and effective

--- hackage/balances/balances.hs
+++ hackage/balances/balances.hs
@@ -1,25 +1,84 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-20 --package "random text Decimal hledger-lib"
-import Import;p=putStrLn;q=Type[Asset,Liability];main=defaultJournal>>=gu.fst.
- balanceReport defreportspec{_rsQuery=q};gu[]=p"";gu bs=do{i<-randomRIO(0,length
-bs-1);let{(a,_,_,b0)=bs!!i};putStr$"\n"++unpack a++" balance ? ";g<-getLine;let{
-b=mixedAmountStripPrices b0;q=maximum$0:map(abs.aquantity)(amounts b);(d,e)=er(
-read g)q;f=roundTo 1e;s=100-min 100f};if d/=0then p("Off by ~"++show d++
-", score: "++show s)>>gu bs else p$"Correct! It is "++showMixedAmountOneLine b++
-".";};er g b=let d=abs$round g-round b in(d,fromIntegral d*100/b+0.001);av as=
- sum as/if null as then 1else fromIntegral(length as)
+-- stack script --resolver lts-20 --package "random text hledger-lib"
+import Import;p=putStrLn;main=defaultJournal>>=g.fst.balanceReport defreportspec
+ {_rsQuery=Type[Cash,Liability]};g bs=if null bs then p"no balances" else do{i<-
+randomRIO(0,length bs-1);let{(a,_,_,b0) = bs !! i};u<-p(unpack a++" balance ? ")
+>>getLine;let{b=mixedAmountStripPrices b0;q=maximum$0:map(abs.aquantity)(amounts
+b);e=c(read u)q};p$(if e<=10 then "✅ " else "❌ ")++(if e<1 then "right!" else
+show e++"% off")++", balance is "++showMixedAmountOneLine b;g bs;}
+c u b=let d=abs$round u-round b in round$fromIntegral d*100/b+0.001
+
 -- ^10 ------------------------------------------------------------------ 80> --
 {- 
 hackage-10-80/balances (sm)
-Tests/drills your financial situational awareness, asking for the balance
-in random asset/liability accounts until you get one right.
+This "game" tests/drills your financial situational awareness, asking hledger
+for the balance in random cash/liability accounts. Not super fun but could be
+a useful practice, since it's easy to keep detailed records in hledger and yet
+still be unclear about current balances.
+
 Uses your default hledger journal if you have one:
 
 $ ./balances.hs
   
-otherwise finance data in a hledger-readable format, from somewhere else:
+otherwise hledger-readable data from elsewhere:
 
-$ LEDGER_FILE=~/src/hledger/examples/sample.journal ./balances.hs
+$ curl -O https://raw.githubusercontent.com/simonmichael/hledger/master/examples/templates.journal
+$ export LEDGER_FILE=$PWD/templates.journal
+
+For each requested account balance, enter a positive integer
+(for multicurrency balances, just guess the most numerous currency).
+Within 10% is a success, within 1% is perfect.
+Control-d to end. An example:
+
+$ hledger bs type:CL   # optional; review balances
+Balance Sheet 2013-01-31
+
+                                           || 2013-01-31 
+===========================================++============
+ Assets                                    ||            
+-------------------------------------------++------------
+ assets:bank:bank1:checking                ||     $25750 
+ assets:bank:bank1:prepaid revenue:client1 ||      $8000 
+ assets:bank:checking                      ||       $-50 
+-------------------------------------------++------------
+                                           ||     $33700 
+===========================================++============
+ Liabilities                               ||            
+-------------------------------------------++------------
+ liabilities:prepaid revenue:client1       ||      $8000 
+ liabilities:tax:federal:2010              ||      $3750 
+ liabilities:tax:federal:2011              ||      $3750 
+ liabilities:tax:state:2010                ||       $900 
+ liabilities:tax:state:2011                ||      $1200 
+-------------------------------------------++------------
+                                           ||     $17600 
+===========================================++============
+ Net:                                      ||     $16100 
+$ clear
+$ ./balances.hs        # test your recall
+assets:bank:checking balance ? 
+8000
+❌ 15900% off, balance is $-50
+liabilities:tax:federal:2011 balance ? 
+3000
+❌ 20% off, balance is $-3750
+liabilities:tax:state:2011 balance ? 
+1000
+❌ 17% off, balance is $-1200
+liabilities:tax:federal:2010 balance ? 
+4000
+✅ 7% off, balance is $-3750
+assets:bank:checking balance ? 
+25000
+❌ 49900% off, balance is $-50
+liabilities:tax:state:2010 balance ? 
+1200
+❌ 33% off, balance is $-900
+assets:bank:bank1:prepaid revenue:client1 balance ? 
+8000
+✅ right!, balance is $8000
+assets:bank:checking balance ? 
+balances.hs: <stdin>: hGetLine: end of file
 
 Unminified version: balances.dev.hs.
 -}
