a242eab5222fff325f60e8a53256c3a04c6f24c1
Gergo ERDI <gergo@erdi.hu>
2023-02-23 21:15:32 +0800

    Import cleanup

--- hackage/subpar-hexagon/Import.hs
+++ hackage/subpar-hexagon/Import.hs
@@ -1,17 +1,11 @@
 module Import
   ( module Graphics.Gloss.Interface.Pure.Game
   , module Data.Bits
-  , module Control.Applicative
   , module Control.Monad
-  , module Data.List
   , module Data.Bool
-  , module GHC.Float
   ) where
 
 import Graphics.Gloss.Interface.Pure.Game
 import Data.Bits (xor)
-import Control.Applicative
-import Data.List(partition)
 import Control.Monad(join)
 import Data.Bool
-import GHC.Float (int2Float)
