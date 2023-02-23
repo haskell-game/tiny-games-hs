deb1d08865ce84bf9036580f0480a1d71201465f
Gergo ERDI <gergo@erdi.hu>
2023-02-23 20:53:19 +0800

    Not so super Hexagon

---
+++ hackage/subpar-hexagon/Import.hs
@@ -0,0 +1,17 @@
+module Import
+  ( module Graphics.Gloss.Interface.Pure.Game
+  , module Data.Bits
+  , module Control.Applicative
+  , module Control.Monad
+  , module Data.List
+  , module Data.Bool
+  , module GHC.Float
+  ) where
+
+import Graphics.Gloss.Interface.Pure.Game
+import Data.Bits (xor)
+import Control.Applicative
+import Control.Monad(guard, mzero)
+import Data.List(partition)
+import Data.Bool
+import GHC.Float (int2Float)
