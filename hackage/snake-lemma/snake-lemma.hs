2676a74e6a8463ec0a256de9e2e11d191bb6c367
Gergo ERDI <gergo@erdi.hu>
2023-02-24 18:14:31 +0800

    Snake Lemma: Address TH splice warning

--- hackage/snake-lemma/snake-lemma.hs
+++ hackage/snake-lemma/snake-lemma.hs
@@ -4,9 +4,9 @@ import Import;main=(b=<<)$playGameS$Game 10((12,)<$>[10..16],(0,-1),(16,21),99,0
  d@(j,i),f,r,z) e|h==f=c((v,u):s,d',(v,u),r,z+1)|o=c((v,u):init s,d',f,r,z)where
  {h@(v,u)=(y+j,x+i);d'|KeyPress k<-e=m d k|o=d};m(0,_)'i'=(-1,0);m(_,0)'j'=(0,-1)
 m(0,_)'k'=(1,0);m(_,0)'l'=(0,1);m d _=d;g _(s,_,f,_,z)=mergePlanes(box 80 24 '█')
- $((2,2),blankPlane 78 22):((2*)<$>f,word$u z):[((2*)<$>s,word w)|(w,s)<-zip("⌀⌀"
- :repeat "▒▒")s];b(_,_,_,_,z)=printf"You proved %d theorems!\n"z;o=True;c q@(s,d,
- f,r,z)|f`elem`s=c(s,d,(2+y`rem`20,2+x),r',z)|o=q where{r'=r`div`2`xor`bool 0
+ $ ((2,2),blankPlane 78 22):((2*)<$>f,word$u z):[((2*)<$>s,word w)|(w,s)<-zip(
+ "⌀⌀":repeat "▒▒")s];b(_,_,_,_,z)=printf"You proved %d theorems!\n"z;o=True;c q@(
+ s,d,f,r,z)|f`elem`s=c(s,d,(2+y`rem`20,2+x),r',z)|o=q where{r'=r`div`2`xor`bool 0
  0xe08(odd r);(y,x)=r`divMod`38};u z=take 2.drop (3*z).cycle$"∀∃⊥⊢⊤⊨∨∧⇒λ→ΣΠη"
 -- ^10 ------------------------------------------------------------------ 80> --
 {- hackage-10-80/snake-lemma (gergoerdi)
