dcb8365262734cd44fe316143bdfd158c508b778
Gergo ERDI <gergo@erdi.hu>
2023-02-24 18:31:52 +0800

    Add scoring

--- hackage/subpar-hexagon/subpar-hexagon-clean.hs
+++ hackage/subpar-hexagon/subpar-hexagon-clean.hs
@@ -7,41 +7,46 @@ type State =
     , Float  -- ^ "Enemy" size
     , [Bool] -- ^ "Enemy" pattern
     , Int    -- ^ PRNG
+    , Int    -- ^ Score
     )
 
 s0 :: State
-s0 = (0, 0, 20, cycle (False:replicate 5 True), 1)
+s0 = (0, 0, 20, cycle (False:replicate 5 True), 1, 0)
 
 main = play FullScreen white 60 s0 paint react step
 
 hex :: [Bool] -> Picture
-hex sides = foldMap (\(p1, p2) -> line [p1, p2]) [p | (p, b) <- zip ps sides, b]
+hex sides = foldMap (\(p1, p2) -> line [p1, p2]) [p | (p, b) <- take 6 $ zip ps sides, b]
   where
     ps = zip hexPoints (tail hexPoints)
 
 hexPoints :: [Point]
-hexPoints = cyle [(-1, 0), (-0.5, 0.87), (0.5, 0.87), (1, 0), (0.5,-0.87), (-0.5,-0.87)]
+hexPoints = cycle [(-1, 0), (-0.5, 0.87), (0.5, 0.87), (1, 0), (0.5,-0.87), (-0.5,-0.87)]
 
 paint :: State -> Picture
-paint (p, r, enemySize, enemyPattern, rng) = rotate r $ scale s s $
-    hex (repeat True) <>
-    rotate 30 (uncurry translate (hexPoints!!p) $ circle 0.1) <>
-    scale enemySize enemySize (hex enemyPattern)
+paint (p, r, enemySize, enemyPattern, rng, score) =
+    scale 5 5 $ rotate r $
+    (translate 20 20 (rotate (-r) (text (show score))) <>) $
+      scale s s $
+        hex (repeat True) <>
+        rotate 30 (uncurry translate (hexPoints!!p) $ circle 0.1) <>
+        scale enemySize enemySize (hex enemyPattern)
   where
-    s = 100 + 20 * sin (0.17 * r)
+    s = 20 + 4 * sin (0.17 * r)
 
 react :: Event -> State -> State
-react (EventKey key Down _ _) (p, r, enemySize, enemyPattern, rng)
-  | key == Char 'j' = ((p - 1)`mod`6, r, enemySize, enemyPattern, rng)
-  | key == Char 'l' = ((p + 1)`mod`6, r, enemySize, enemyPattern, rng)
+react (EventKey key Down _ _) (p, r, enemySize, enemyPattern, rng, score)
+  | key == Char 'j' = ((p - 1)`mod`6, r, enemySize, enemyPattern, rng, score)
+  | key == Char 'l' = ((p + 1)`mod`6, r, enemySize, enemyPattern, rng, score)
 react _ s = s
 
 step :: Float -> State -> State
-step dt (p, r, enemySize, enemyPattern, rng)
+step dt (p, r, enemySize, enemyPattern, rng, score)
     | enemySize > 1
-    = (p, r + dt * 90, enemySize * 0.2**dt, enemyPattern, rng')
-    -- = (p, r + dt * 90, enemySize - 10*dt, enemyPattern, rng')
-    | not (enemyPattern!!p)
-    = (p, r + dt * 90, 20, drop rng enemyPattern, rng')
+    = (p, r + dt * 90, enemySize * 0.2**dt, enemyPattern, rng', score)
+    -- = (p, r + dt * 90, enemySize - 10*dt, enemyPattern, rng', score)
+    | otherwise
+    = (p, r + dt * 90, 20, drop rng enemyPattern, rng', if hit then 0 else score + 1)
   where
     rng' = rng `div` 2 `xor` bool 0 96 (odd rng)
+    hit = enemyPattern!!p
