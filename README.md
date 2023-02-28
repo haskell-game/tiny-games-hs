53c360b1cd16126a6d71ca79571a8e9155576425
Tristan Cacqueray <tdecacqu@redhat.com>
2023-02-28 19:43:52 +0000

    Add quine

--- README.md
+++ README.md
@@ -108,6 +108,7 @@ Here are the entries received so far!
 <td><img src="prelude/call-by-push-block/call-by-push-block.gif" width=140 height=140><br><a href="prelude/call-by-push-block">call-by-push-block</a><br>(cole-k)</td>
 <td><a href="prelude/companion"><img src="prelude/companion/companion.png" width=140 height=140><br>companion</a><br>(Greg8128)</td>
 <td><a href="prelude/hangman"><img src="prelude/hangman/hangman.png" width=140 height=140><br>hangman</a><br>(kukimik)</td>
+<td><a href="prelude/quine"><img src="prelude/quine/quine.png" width=140 height=140><br>quine</a><br>(tristanC)</td>
 </tr>
 </table>
 
---
+++ prelude/quine/quine.hs
@@ -0,0 +1,10 @@
+#!/usr/bin/env runghc
+a b|b-97<26&&b-97>=0=97+rem(b-84)26|o=b;q=t 34;e=t 92;n=t 10;o=True;t=toEnum
+c=n:"-- prelude-10-80/quine (tristanC)";d='-';b('#':'!':y)=q:'#':'!':b y
+b[]=q:c;b(x:y)|f x==10=e:'n':e:n:e:b y|f x==34=e:q:b y|o=x:b y
+f=fromEnum;main=putStrLn((t.a.f<$>s)++b s)where s="#!/hfe/ova/rai ehatup\n\
+\n o|o-97<26&&o-97>=0=97+erz(o-84)26|b=o;d=g 34;r=g 92;a=g 10;b=Tehr;g=gbEahz\n\
+\p=a:\"-- ceryhqr-10-80/dhvar (gevfgnaC)\";q='-';o('#':'!':l)=d:'#':'!':o l\n\
+\o[]=d:p;o(k:l)|s k==10=r:'a':r:a:r:o l|s k==34=r:d:o l|b=k:o l\n\
+\s=sebzEahz;znva=chgSgeLa((g.n.s<$>f)++o f)jurer f="
+-- prelude-10-80/quine (tristanC)
