55fa228cc7dd656659503b508a548d949f975d84
Joshua Price <elderephemera@gmail.com>
2023-02-28 04:21:41 -0500

    Update default/shmupemup

--- default/shmupemup/Import.hs
+++ default/shmupemup/Import.hs
@@ -1,4 +1,4 @@
-module Imports (module X) where
+module Import (module X) where
 
 import Data.Char as X (chr)
 import Data.Foldable as X (asum, fold)
@@ -6,5 +6,6 @@ import Data.Functor as X (($>))
 import Data.List as X (transpose)
 import GHC.Clock as X (getMonotonicTimeNSec)
 import GHC.Conc as X (threadDelay)
+import GHC.Utils.Misc as X (changeLast, count)
 import System.IO as X (hReady, hSetBuffering, hSetEcho, BufferMode(..), stdin)
 import Text.Parsec as X ((<|>), anyChar, eof, many, oneOf, parse, string, try)
--- default/shmupemup/shmupemup.hs
+++ default/shmupemup/shmupemup.hs
@@ -1,28 +1,30 @@
-{import Imports;main=do{hSetBuffering i NoBuffering;hSetEcho i(1<0);0!(('>':r 79
-' '):r 9(r 80' '))};l q|q=getChar<*g;l _=pure '#';m=asum.map try;g=l=<<hReady i;
-n=string;k=pure<$>e;i=stdin;t=transpose;n!s=do{putStr$"\27[2J"++unlines(init<$>s
-)++"Score: ";print n;w<-getMonotonicTimeNSec;threadDelay 50000;p<-g;(n+sum[1|'X'
-<-fold$init<$>s])!(zipWith(\x->(++[x]).init.(b$h p))(chr.(`mod`150).fromIntegral
-<$>iterate(0xe817fb2d*)w).t.map(b$v p)$t s)};h 'a'=" >"%j#h;h 'd'=j%" >"#h;h ' '
-=j%">-"#h;h _=m["- "%" X "<*f,(:" ")<$>(n" "*>f),"-"%" X"<*f,">"%"X "<*f,f$>" ",
-n"-"*>e$>" -",k];e=anyChar;v 's'=j%" >"#v;v 'w'=" >"%j#v;v _=m["X"%" ","/>"%" X"
-,n"/"*>e$>" /","/"%w<*eof,">\\"%"X ",e*>w%"\\ ",w%"/",k];j="> ";r=replicate;b r=
-fold.fold.parse(many r)"";p#f=try p<|>f '#';x%y=n x$>y;w="\\";f=oneOf"{[@\\/"}--
+{import Import;m=asum.map try;main=do{hSetBuffering i NoBuffering;hSetEcho i(1<0
+);0!(('>':r 79' '):r 9(r 80' '))};l q|q=getChar<*g;l _=pure '#';g=l=<<hReady i;n
+=string;k=pure<$>e;n!s=do{putStr$"\27[2J"++unlines(init<$>s)++"Score: ";print n;
+w<-getMonotonicTimeNSec;threadDelay 50000;p<-g;(n+c 'X's)!zipWith(changeLast.b(h
+p$c '-'s<9))(t.map(b$v p)$t s)(chr.fromEnum.(`mod`150)<$>iterate(0xe817fb2d*)w)}
+;c x=count(x==).(init=<<);h ' 'f|f=j%">-"#y;h 'a'_=z%j#y;h 'd'_=j%z#y;h _ _=y;y=
+m["- "%" X "<*f,(:u)<$>(n u*>f),"-"%" X"<*f,">"%"X "<*f,f$>u,"-"%" -"<*e,k];b r=
+fold.fold.parse(many r)"";e=anyChar;v 's'=j%z#p;v 'w'=z%j#p;v _=p;p=m["X"%u,"/>"
+%" X","/"%" /"<*e,"/"%w<*eof,">\\"%"X ",e*>w%"\\ ",w%"/",k];j="> ";z=" >";u=" ";
+p#f=try p<|>f;x%y=n x$>y;w="\\";i=stdin;t=transpose;r=replicate;f=oneOf"{[@\\/"}
 -- ^10 ------------------------------------------------------------------ 80> --
 {- default-10-80/shmupemup (elderephemera), ghc 9.2.5
 
 A terminal shmup game. Tap the WASD keys to move and Space to fire. Shoot down
-as many enemy space ships as you can before one hits you!
+as many enemy space ships as you can before one hits you. Be careful not to let
+your gun overheat!
 
 On Linux and MacOS (untested, but it should work) you can run the game with
-`runghc shmupemup.hs` or if that doesn't work, with `stack runghc --package
-parsec shmupemup.hs`. On Windows, because of GHC issue #2189, the game must be
-compiled with the native io manager which you can do with the command `ghc
--with-rtsopts --io-manager=native shmupemup.hs`. You will also have to enable
-VT100 support if you don't want the screen to be repeated which you can do by
-running `reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1`.  There
-is also jitter on Windows, Command Prompt seems to have less jitter than
-PowerShell or Windows Terminal though.
+`runghc -package=ghc shmupemup` or if that doesn't work, with `stack runghc
+--resolver lts-20.12 --package ghc-9.2.6 --package parsec shmupemup`. On
+Windows, because of GHC issue #2189, the game must be compiled with the native
+io manager which you can do with the command `ghc -with-rtsopts
+--io-manager=native shmupemup.hs`. You will also have to enable VT100 support if
+you don't want the screen to be repeated which you can do by running `reg add
+HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1`.  There is also jitter
+on Windows, Command Prompt seems to have less jitter than PowerShell or Windows
+Terminal though.
 
 The game state is just a list of lines (i.e. `[String]`) that is transformed
 every frame by two Parsec parsers, one for horizontal interactions, and one for
@@ -49,66 +51,76 @@ import Data.Functor (($>))
 import Data.List (transpose)
 import GHC.Clock (getMonotonicTimeNSec)
 import GHC.Conc (threadDelay)
+import GHC.Utils.Misc (changeLast, count)
 import System.IO (hReady, hSetBuffering, hSetEcho, BufferMode(..), stdin)
 import Text.Parsec ((<|>), anyChar, eof, many, oneOf, parse, string, try)
 
 main = do
   hSetBuffering stdin NoBuffering
-  hSetEcho stdin False
-  loop 0 $ ('>' : replicate 79 ' ') : replicate 9 (replicate 80 ' ')
+  hSetEcho stdin (1 < 0)
+  loop 0 (('>' : replicate 79 ' ') : replicate 9 (replicate 80 ' '))
 
 -- Renamed: !
-loop n s=do
-  putStr $ "\27[2J" ++ unlines (init <$> s) ++ "Score: "
-  print n;
-  time <- getMonotonicTimeNSec;
-  threadDelay 50000;
-  input <- getInput;
-  loop
-    (n + sum [ 1 | 'X' <- fold $ init <$> s ])
-    (zipWith (\x-> (++[x]) . init . (applyParser $ horizontal input))
-      (chr . (`mod` 150) . fromIntegral <$> iterate (0xe817fb2d*) time)
-      (transpose . map (applyParser $ vertical input) $ transpose s))
+loop score screen = do
+  putStr $ "\27[2J" ++ unlines (init <$> screen) ++ "Score: "
+  print score
+  time <- getMonotonicTimeNSec
+  threadDelay 50000
+  input <- getInput
+  loop (score + countOnScreen 'X'screen)
+    $ zipWith
+      (changeLast . runParser (horizontal input $ countOnScreen '-' screen < 9))
+      (transpose . map (runParser $ vertical input) $ transpose screen)
+    $ chr . fromEnum . (`mod` 150) <$> iterate (0xe817fb2d *) time
 
 -- Renamed: g
-getInput = getInput' =<< hReady stdin;
-
+getInput = go =<< hReady stdin where
 -- Renamed: l
-getInput' ready | ready = getChar <* getInput;
-getInput' _ = pure '#';
+  go ready | ready = getChar <* getInput
+  go _ = pure '#'
+
+-- Renamed: c
+countOnScreen char = count (char ==) . (init =<<)
 
 -- Renamed: b
-applyParser r = fold . fold . parse (many r) "";
+runParser parser = fold . fold . parse (many parser) ""
+
+-- Renamed: m
+options = asum . map try
+
+p # f = try p <|> f
 
 -- Renamed: %
-x --> y = string x $> y;
-
-p # f = try p <|> f '#';
+x --> y = string x $> y
 
 -- Renamed: f
 enemy = oneOf "{[@\\/"
 
 -- Renamed: h
-horizontal 'a' = " >" --> "> " # horizontal
-horizontal 'd' = "> " --> " >" # horizontal
-horizontal ' ' = "> " --> ">-" # horizontal
-horizontal _ = asum $ map try
+horizontal ' ' fire | fire = "> " --> ">-" # horizontal'
+horizontal 'a' _ = " >" --> "> " # horizontal'
+horizontal 'd' _ = "> " --> " >" # horizontal'
+horizontal _ _ = horizontal'
+-- Renamed: y
+horizontal' = options
   [ "- " --> " X " <* enemy
   , (:" ") <$> (string " " *> enemy)
   , "-" --> " X" <* enemy
   , ">" --> "X " <* enemy
   , enemy $> " "
-  , string "-" *> anyChar $> " -"
+  , "-" --> " -" <* anyChar
   , (:[]) <$> anyChar
   ]
 
 -- Renamed: v
-vertical 's' = "> " --> " >" # vertical
-vertical 'w' = " >" --> "> " # vertical
-vertical _ = asum $ map try
+vertical 's' = "> " --> " >" # vertical'
+vertical 'w' = " >" --> "> " # vertical'
+vertical _ = vertical'
+-- Renamed: p
+vertical' = options
   [ "X" --> " "
   , "/>" --> " X"
-  , string "/" *> anyChar $> " /"
+  , "/" --> " /" <* anyChar
   , "/" --> "\\" <* eof
   , ">\\" --> "X "
   , anyChar *> "\\" --> "\\ "
