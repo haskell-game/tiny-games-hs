4b47016e46414c42745cc8f4c2bf242cb21bb07c
Tristan Cacqueray <tdecacqu@redhat.com>
2023-02-28 21:42:12 +0000

    Add license and line separator to the quine

--- prelude/quine/quine.hs
+++ prelude/quine/quine.hs
@@ -1,10 +1,14 @@
 #!/usr/bin/env runghc
-a b|b-97<26&&b-97>=0=97+rem(b-84)26|o=b;q=t 34;e=t 92;n=t 10;o=True;t=toEnum
-c=n:"-- prelude-10-80/quine (tristanC)";d='-';b('#':'!':y)=q:'#':'!':b y
-b[]=q:c;b(x:y)|f x==10=e:'n':e:n:e:b y|f x==34=e:q:b y|o=x:b y
-f=fromEnum;main=putStrLn((t.a.f<$>s)++b s)where s="#!/hfe/ova/rai ehatup\n\
-\n o|o-97<26&&o-97>=0=97+erz(o-84)26|b=o;d=g 34;r=g 92;a=g 10;b=Tehr;g=gbEahz\n\
-\p=a:\"-- ceryhqr-10-80/dhvar (gevfgnaC)\";q='-';o('#':'!':l)=d:'#':'!':o l\n\
-\o[]=d:p;o(k:l)|s k==10=r:'a':r:a:r:o l|s k==34=r:d:o l|b=k:o l\n\
-\s=sebzEahz;znva=chgSgeLa((g.n.s<$>f)++o f)jurer f="
--- prelude-10-80/quine (tristanC)
+q=t 34;e=t 92;n=t 10;d='-';t=toEnum;z="-- ^10 ";b('#':'!':y)=q:'#':'!':b y
+b[]=q:c;b(x:y)|x==n=e:'n':e:n:e:b y|x==q=e:q:b y|o=x:b y;l="SPDX: CC0-1.0"
+s=n:z++[d|_<-[0..66]]++" 80> --"++n:"{- prelude-10-80/quine (tristanC)"
+o=True;c=n:s++n:l++n:"-}";main=putStrLn(s++b s)where s="#!/usr/bin/env runghc\n\
+\q=t 34;e=t 92;n=t 10;d='-';t=toEnum;z=\"-- ^10 \";b('#':'!':y)=q:'#':'!':b y\n\
+\b[]=q:c;b(x:y)|x==n=e:'n':e:n:e:b y|x==q=e:q:b y|o=x:b y;l=\"SPDX: CC0-1.0\"\n\
+\s=n:z++[d|_<-[0..66]]++\" 80> --\"++n:\"{- prelude-10-80/quine (tristanC)\"\n\
+\o=True;c=n:s++n:l++n:\"-}\";main=putStrLn(s++b s)where s="
+
+-- ^10 ------------------------------------------------------------------- 80> --
+{- prelude-10-80/quine (tristanC)
+SPDX: CC0-1.0
+-}
