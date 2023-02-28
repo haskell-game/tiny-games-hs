dc1111ab4a8098080a4c6e19cd894bb72bb45ee2
Cole K <cole-k@users.noreply.github.com>
2023-02-27 23:57:09 -0800

    update call-by-push-block

--- prelude/call-by-push-block/Readme.md
+++ prelude/call-by-push-block/Readme.md
@@ -11,8 +11,8 @@ Your score is the number of moves you take. Like in real golf, a lower
 score is better, but make sure you can complete the level first before you
 try to get the best score.
 
-There are 14 levels of increasing difficulty. They will take around an hour
-to complete, depending on experience. Your scores for each level are given
+There are 15 levels of roughly increasing difficulty. They will take around an
+hour to complete, depending on experience. Your scores for each level are given
 at the end: compete with your friends to see who can get the lowest scores!
 
 ## External links
@@ -30,6 +30,7 @@ the Prelude category).
 - Try everything! You can always undo or reset if you reach an unsolvable
 state.
 - Read the [Controls in detail](#controls-in-detail) section if you're confused by the controls.
+- Read the "Hints" section if you get stuck.
 
 ## The cast
 
@@ -43,7 +44,8 @@ state.
 - Your score is displayed beneath the level number, starting at 1.
 - It increments for every move and undo you make and resets whenever you
 reset the level.
-- A lower score is better.
+- A lower score is better, except for a score of 0 which indicates an
+  incompleted level.
 
 ## Controls in detail
 
@@ -61,6 +63,72 @@ Note that you need to press Enter in order to make a move.
     you were if you make a mistake) and incurs a small score penalty.
 - Additional notes
    - When prompted with `↩`, press Enter to continue.
-   - Technically, all keys map to one of the above controls. If you find that
-     your game is behaving in unexpected ways, you might be pressing keys
-     outside of the prescribed ones.
+   - Technically, all keys map to a control. Try to avoid pressing an
+     unlabeled key as you can accidentally skip a level this way.
+
+
+## Hints
+***WARNING**: Do not read this in plaintext, as the hints are "hidden" using
+`<details>` tags. Read the comments in `call-by-push-block.hs` for rot-13
+enciphered hints.
+
+I recommend revealing them in order until you find one that helps. If you
+are completely stuck on a level, there is a level skip code at the end.
+
+- Levels 1-5
+  - <details><summary>Spoiler</summary>You can direct the player character off
+    the grid. This will require you to reset in order to beat the
+    level.</details>
+  - <details><summary>Spoiler</summary>The hash is a static obstacle. Like a
+    hole, it cannot be moved or passed through. You also cannot push blocks into
+    it.</details>
+  - <details><summary>Spoiler</summary>You can push multiple blocks at a
+    time.</details>
+  - <details><summary>Spoiler</summary>You can push blocks off of the grid. Be
+    careful not to push a block you need off the grid.</details>
+  - <details><summary>Spoiler</summary>You need an empty tile adjacent to a
+    block in order to push it. For example, to push a block down, it needs an
+    empty tile above it. Keep this in mind even for later levels since it will
+    allow you to identify holes that are inaccessible to certain
+    blocks.</details>
+- Levels 5-8
+  - <details><summary>Spoiler</summary>The first hint is no longer true: there
+    are conditions where pushing a player character off the grid is necessary.
+    Remember: you only need to push a block into each hole.</details>
+  - <details><summary>Spoiler</summary>The order you try things in often
+    matters. Sometimes you may have the right idea but wrong execution
+    order.</details>
+  - <details><summary>Spoiler</summary>Reset or undo early if you realize you
+    are in an unwinnable state. A quick trick to determine whether you are in
+    one is to count the number of remaining holes and check if there is a block
+    that can be pushed into each. Refer also to the last hint of the previous
+    section.</details>
+- Levels 9-15
+  - <details><summary>Spoiler</summary>The plus is a tile that destroys both
+    player characters and blocks alike. When a block is pushed into it or a
+    player character moves into it, both the plus and the object encountering it
+    are replaced with an empty tile.</details>
+  - <details><summary>Spoiler</summary>Take advantage of hashes or holes to move
+    some player characters will keeping others stationary.</details>
+  - <details><summary>Spoiler</summary>Elaborating on the above hint, you will
+    find that some things which seem impossible are only so because your player
+    characters are not coordinated in space properly. Sometimes you need to
+    coordinate them by moving one and keeping another stationary. Other times
+    they will be separated in an unwinnable state.</details>
+- Level skip cheat code
+  - <details><summary>Spoiler</summary>Type "Cheat Code" (case sensitive) and
+    press enter to skip the level. This will confer a score of 0, indicating the
+    level was skipped. If you are playing on the unbuffered version, just typing
+    the first letter will suffice.</details>
+  
+## Acknowledgements
+- Golfing: I've learned a bunch about code golfing in Haskell from various
+tips and threads I've read from inumerably many people. I got some
+explicit help from The Ninteenth Byte on Stack Exchange with shaving
+characters off. Thank you!
+- Testing and design: I solicited help from a bunch of people who
+graciously lent me their time and feedback. Thank you!
+- The jam and feedback: #haskell-game and the organizers of The Haskell Tiny
+  Game Jam. Thank you!
+- Playing the game in the browser: [Radon](https://github.com/raxod502) and
+  [Radian](https://radian.codes/) for creating and hosting Riju.
--- prelude/call-by-push-block/call-by-push-block.hs
+++ prelude/call-by-push-block/call-by-push-block.hs
@@ -1,13 +1,13 @@
-n="噞㋪㛃䚬摣ۜ㝜ቲ㜑昫⫛㍋勓ㅋ⩞䤤㱪㳃䊩㓛⤤㳝ᙪ㛅Ⳝ㩛㚙㛦䥢㔬㜞㋖⛍㣛戛㙉ኛ㙎ኛ孶抭安Ჭ㱉㚛㛛ᰛ⯙瞎㤅⛛牶䢯㛛㣠⭤㳳㑌䜜㚿㛛䜓缣㛚≣㶳ㅬ㛜㛛䛚㛛暛㛣⛛䤤䣤"
-e('λ':c)|(a,b)<-span(>'n')c=u(u a#m 1 b)%e(y b);e(c:d)=c:e d;e l=l;k="λ.";m=take
-v(x:_)=c x`mod`7;v _=0;l=print;s=[]:s;c=fromEnum;i="\no .#_λ+";y=drop 1;(%)=(++)
-e&0=u.t;f&_=f;p=putStr;(1?f)x=z x;(5?f)x=y x%z x%z x;(i?f)x=(f&i$r e$f$head x):x
-r=map;main=print=<<(mapM g.zip[1..].r(pure.words).lines$r(i!!).m 5.w=<<r c(n%x))
-g(k,o)|any(elem '_')$head o=do{k!o;p b;i<-v<$>d;g(k,i?([t.u,t,id,t,t,t,r u]!!i)$
-o)};g(k,x)=a x<$k!x<*p"↩"<*d;t=foldr(zipWith(:))s;z=m 1.u;w n=n`mod`8:w(n`div`8)
-n!x@(s:_)=p"\^[cLvl "*>l n*>l(a x)*>p(unlines s);d=getLine;(_:r)#""=r%k;_#""="."
-n#"."=n%k;(_:r)#"+"='.':r%k;_#"+"="..";('o':r)#"_"='O':r%k;l#x=x%l%"λ";u=reverse
-a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛⛛笛⛛禣✤ባ⦉档✌䡣✌ቴ✙䤣✴㛛ۣ廛⛟盻⟛㛛⛛䌜⣡㞛⛳㣣✜幜⣏㛜ࣛ"
+n="晘㋲㛻嚵ѫ盝㞝ቂ㝑߳ⳛ㎋拓㾋ⱘ孭ひ・励㓛⭭マᙲ㛾⃝㱛㚙㛨宪㕵㝘㋐⛎㫛ϛ㙉ኛ㙈ኛ涀ʶ氉Ⴖぉ㚛㛛ᇛⴙ䘈㭾⛛䉀媴㛛㫯ⶭッ㑍坝㚤㛛坓䥫㛚≫〃㾵㛝㛛囚㛛ڛ㛫⛛孭嫭"
+e('λ':c)|(a,b)<-span(>'n')c=u(u a#m 1 b)%e(y b);e(c:d)=c:e d;e l=l;e&0=u.v;f&_=f
+v=foldr(zipWith(:))a;main=print=<<(mapM o.zip[1..].r(h.words).lines$m 5.w.c=<<x)
+e%f=e<>f;p=putStr;l=length;w n="λo .+#_\n"!!mod n 8:w(n`div`8);d=getLine;z=m 1.u
+r=map;1?f=z;4?f=h[];5?f=y%z%z;i?f=(h.(f&i).r e.f.head)%id;q(x:_)=c x`mod`7;q _=0
+g=print;o(n,x)|x==[]=h 0|any(elem '_')$x!!0=do{n!x;p"λ:wasd 🔄:x 🔙:u\n";i<-q<$>d;
+o(n,i?([v.u,v,id,v,v,v,r u]!!i)$x)}|0<3=l x<$n!x<*p"↩"<*d;k="λ.";y=drop 1;m=take
+n!x@(s:_)=p"\^[cLvl "*>g n*>g(l x)*>p(unlines s);c=fromEnum;[]#""=".";l#""=y l%k
+n#"."=n%k;[]#"+"="..";l#"+"='.':y l%k;('o':r)#"_"='O':r%k;l#x=x%l%"λ";h x=pure x
+a=[]:a;u=reverse;x=n%"ᅕ䤉喤孭坫㛚㫛㪛囃盃㞛⛳䞛⛳䨫❭ቫ⨉੫❍婫❍ቅ❙孫❅㛛盫棛⛜䛣✛㛛⛛卝⫩㘛⛃㫫❝桝⫌㛝竛㝭㛛㪛㛰㛃ᝪ㭫哈坛ë㝓坝烰"
 -- ^10 ------------------------------------------------------------------ 80> --
 {- gam-10-80-hs-prelude/call-by-push-block (cole-k), ghc 9.2.5
 
@@ -21,9 +21,10 @@ a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛�
      score is better, but make sure you can complete the level first before you
      try to get the best score.
 
-     There are 14 levels of increasing difficulty. They will take around an hour
-     to complete, depending on experience. Your scores for each level are given
-     at the end: compete with your friends to see who can get the lowest scores!
+     There are 15 levels of roughly increasing difficulty. They will take around
+     an hour to complete, depending on experience. Your scores for each level
+     are given at the end: compete with your friends to see who can get the
+     lowest scores!
 
    Running:
      - This program requires no additional arguments and can be run using
@@ -35,6 +36,7 @@ a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛�
      - Try everything! You can always undo or reset if you reach an unsolvable
        state.
      - Read the "Controls in detail" section if you're confused by the controls.
+     - Read the "Hints" section if you get stuck.
 
    The cast:
      - λ: The player character.
@@ -46,7 +48,8 @@ a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛�
      - Your score is displayed beneath the level number, starting at 1.
      - It increments for every move and undo you make and resets whenever you
        reset the level.
-     - A lower score is better.
+     - A lower score is better, except for a score of 0 which indicates an
+       incompleted level.
 
    Controls in detail:
      Note that you need to press enter in order to make a move.
@@ -60,9 +63,54 @@ a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛�
          you were if you make a mistake) and incurs a small score penalty.
      - Additional notes
         - You need to press enter in order to make a move.
-        - When prompted with ⮑, press enter to continue.
-        - Technically, all keys map to one of the above controls, so if you find
-        a set of keys you prefer, feel free to use them.
+        - When prompted with ↩, press enter to continue.
+        - Technically, all keys map to a control. Try to avoid pressing an
+          unlabeled key as you can accidentally skip a level this way.
+
+   Hints:
+     These hints all contain spoilers and as such are enciphered using rot-13.
+
+     I recommend revealing them in order until you find one that helps. If you
+     are completely stuck on a level, there is a level skip code at the end.
+     - Levels 1-5
+       - Lbh pna qverpg gur cynlre punenpgre bss gur tevq. Guvf jvyy erdhver lbh
+         gb erfrg va beqre gb orng gur yriry.
+       - Gur unfu vf n fgngvp bofgnpyr. Yvxr n ubyr, vg pnaabg or zbirq be
+         cnffrq guebhtu. Lbh nyfb pnaabg chfu oybpxf vagb vg.
+       - Lbh pna chfu zhygvcyr oybpxf ng n gvzr.
+       - Lbh pna chfu oybpxf bss bs gur tevq. Or pnershy abg gb chfu n oybpx lbh
+         arrq bss gur tevq.
+       - Lbh arrq na rzcgl gvyr nqwnprag gb n oybpx va beqre gb chfu vg. Sbe
+         rknzcyr, gb chfu n oybpx qbja, vg arrqf na rzcgl gvyr nobir vg. Xrrc
+         guvf va zvaq rira sbe yngre yriryf fvapr vg jvyy nyybj lbh gb vqragvsl
+         ubyrf gung ner vanpprffvoyr gb pregnva oybpxf.
+     - Levels 5-8
+       - Gur svefg uvag vf ab ybatre gehr: gurer ner pbaqvgvbaf jurer chfuvat n
+         cynlre punenpgre bss gur tevq vf arprffnel. Erzrzore: lbh bayl arrq gb
+         chfu n oybpx vagb rnpu ubyr.
+       - Gur beqre lbh gel guvatf va bsgra znggref. Fbzrgvzrf lbh znl unir gur
+         evtug vqrn ohg jebat rkrphgvba beqre.
+       - Erfrg be haqb rneyl vs lbh ernyvmr lbh ner va na hajvaanoyr fgngr. N
+         dhvpx gevpx gb qrgrezvar jurgure lbh ner va bar vf gb pbhag gur ahzore
+         bs erznvavat ubyrf naq purpx vs gurer vf n oybpx gung pna or chfurq
+         vagb rnpu. Ersre nyfb gb gur ynfg uvag bs gur cerivbhf frpgvba.
+     - Levels 9-15
+       - Gur cyhf vf n gvyr gung qrfgeblf obgu cynlre punenpgref naq oybpxf
+         nyvxr. Jura n oybpx vf chfurq vagb vg be n cynlre punenpgre zbirf vagb
+         vg, obgu gur cyhf naq gur bowrpg rapbhagrevat vg ner ercynprq jvgu
+         na rzcgl gvyr.
+       - Gnxr nqinagntr bs unfurf be ubyrf gb zbir fbzr cynlre punenpgref jvyy
+         xrrcvat bguref fgngvbanel.
+       - Rynobengvat ba gur nobir uvag, lbh jvyy svaq gung fbzr guvatf juvpu
+         frrz vzcbffvoyr ner bayl fb orpnhfr lbhe cynlre punenpgref ner abg
+         pbbeqvangrq va fcnpr cebcreyl. Fbzrgvzrf lbh arrq gb pbbeqvangr gurz ol
+         zbivat bar naq xrrcvat nabgure fgngvbanel. Bgure gvzrf gurl jvyy or
+         frcnengrq va na hajvaanoyr fgngr.
+      - Level skip cheat code
+        - Glcr "Purng Pbqr" (pnfr frafvgvir) naq cerff ragre gb fxvc gur yriry.
+          Guvf jvyy pbasre n fpber bs 0, vaqvpngvat gur yriry jnf fxvccrq. Vs
+          lbh ner cynlvat ba gur haohssrerq irefvba, whfg glcvat gur svefg
+          yetter will suffice.
 
    Acknowledgements :
      - Golfing: I've learned a bunch about code golfing in Haskell from various
@@ -71,4 +119,6 @@ a=length;b="λ:wasd 🔄:x 🔙:u\n";x="ᴔ翉䕿䤤䜣㛚㣛㢛䛳۳四⛛竛�
        characters off. Thank you!
      - Testing and design: I solicited help from a bunch of people who
        graciously lent me their time and feedback. Thank you!
+     - The jam and feedback: #haskell-game and the organizers of The Haskell Tiny
+       Game Jam. Thank you!
 -}
